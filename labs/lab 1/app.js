f05ce374c5af33a3fbaa17d944b92fc84459c7bf
253olivares <67510851+253olivares@users.noreply.github.com>
2020-09-03 23:53:34 -0400

    Rain finished all thats left is to add counter and have the ground change color

--- labs/lab 1/app.js
+++ labs/lab 1/app.js
@@ -13,20 +13,30 @@
 
 
 class Rain{
-    constructor(rx, ry, radius, color){
+    constructor(rx, ry, color){
         this.x = rx;
         this.y = ry;
-        this.length = 15;
-        this.radius = 0;
-        this.color = color;
-        this.speed = 1 + Math.random() * 2;
+        this.length = random(10, 20);
+        this.width = random(3, 5);
+        this.color = 255,255,255;
+        // this.speed = 1 + Math.random() * 2;
     }
 
     update(){
-        this.y = this.y + this.speed;
-        this.speed = this.speed + .2;
-        this.y++
-        circle( this.x, this.y,this.radius);
+        noStroke();
+        fill(this.color);
+        ellipse(this.x, this.y, this.width, this.length);
+        this.y = this.y + 6;
+        // this.y = this.y + this.speed;
+        // this.speed = this.speed + .1;
+        // this.y++
+        if (this.y > window.innerHeight * .925){
+            //sends rain drop back above off screen with reset values
+            this.y = random(0, -3000);
+            this.length = random(10, 20);
+            this.width = random(3, 5);
+            // this.speed = 1 + Math.random() * 2;
+        }
     }
 }
 
@@ -44,23 +54,45 @@ class Ground{
     }
 
     update(){
+        fill('rgb(0%,0%,'+ this.blue +'%)');
         rect(this.xRect, this.yRect, this.width, this.height);
-        fill('rgb(0%,0%,'+ this. blue +'%)');
         }
 }
 
-console.log("Current screen height is " + window.innerHeight);
-console.log("Current screen width is " + window.innerWidth)
+//debugging
+// console.log("Current screen height is " + window.innerHeight);
+// console.log("Current screen width is " + window.innerWidth)
 
+//creating rain array to store dropplets
+var rain =[];
 var ground = new Ground(0 , window.innerHeight * .9, window.innerWidth, window.innerHeight * .1);
 
 
 function setup(){
-    createCanvas(windowWidth, windowHeight);  
+    createCanvas(window.innerWidth, windowHeight);  
+    //Creating rain and putting it in array
+    //I am giving all the rain random x anmd y's out side the canvas so that 
+    //it appears to be falling all at random moments instead of all the rain spawning at once and just falling
+    for (i = 0; i < 200; i++) {
+        rain[i] = new Rain(random(50,window.innerWidth - 50), random(0, -3000));
+      }
+    console.log("This is the rain array that is created     " + rain + "     all this rain exists outside of canvas at start");
 
 }
+
 function draw() {
-    background(208, 211, 212);
+    //sky blue rgb
+    background(142, 188, 212);
     ground.update();
-    ellipse(30, 30, 3, 15);
+    //testing ellipse to determine shape of rain
+    // ellipse(30, 30, 3, 15);
+    for (i = 0; i < rain.length; i++) {
+        rain[i].update();
+      }
+    fill(102, 102, 102);
+    rect(0,0,290,60);
+    triangle(290,0,325,0,290,60 );
+    fill(255,255,255);
+    textSize(32);
+    text("Ground Rain Lab 1", 10, 40);
 }
