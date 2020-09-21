269f3b51ace9978bd5a90b7d655bd54cb648ea0e
253olivares <67510851+253olivares@users.noreply.github.com>
2020-09-21 00:32:29 -0400

    Creating lab3

---
+++ labs/lab 3/app.js
@@ -0,0 +1,29 @@
+function setup(){
+    createCanvas(window.innerWidth, windowHeight); 
+     //This is all just code to create a button and remove its css so that I can put a gon back link to the main page
+    //will have more of these in later labs
+    let button = createButton('Go Back');
+    button.position(10, 70);
+    button.style('font-size', '25px');
+    button.style('background', 'none');
+    button.style('border','none');
+    button.style('color','white');
+    button.style('cursor','pointer');
+    button.mouseClicked(goBack);
+    //this is just to link back to main page
+    function goBack(){
+        window.open('../../index.html', '_self');
+    } 
+}
+function draw(){
+    //copy pasted from previous lab to create go back link and title of this lab
+    noStroke();
+    fill(102, 102, 102);
+    rect(0,0,225,60);
+    rect(0,60,125,50);
+    triangle(225,0,255,0,225,60 );
+    triangle(125,60,160,60,125,110 );
+    fill(255,255,255);
+    textSize(32);
+    text("Templates Lab", 10, 40);
+}
