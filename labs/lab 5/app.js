fcbe9a19243389896993db257d0a68d8b8b7af5d
253olivares <67510851+253olivares@users.noreply.github.com>
2020-10-09 15:24:28 -0400

    added comments

--- labs/lab 5/app.js
+++ labs/lab 5/app.js
@@ -1,9 +1,18 @@
-
+// this is out tween max to help make a fade in animation on the whole page. 
+//All the elements are surounded in a container div that acts as a secondary body tag
+//that div is refrences and told to fade in from alpha 0 in 4 seconds 
+//and a small 500 x displacement to give it a little it of a slide in animation
 TweenMax.from("#container",{duration: 4, alpha:0, x:500});
 
+
+//here I am getting the div element of all the galaxy boxes  and storing them in a array
 let test = document.getElementsByClassName("Box");
+//debug tool to test to see if all the box divs are stored and recognized. 
 console.log(test);
 
+//A loop to give each div a mouse over and mouse out event listener that will stay until they go away with the click command
+//each event listener is given a function with a tween max that will activate on the targeted
+//that clicked using event target
 for (var i = 0; i < test.length; i++) {
     test[i].addEventListener('mouseover', function(event){
         TweenMax.to(event.target,{duration: 1, scale:1.25});
