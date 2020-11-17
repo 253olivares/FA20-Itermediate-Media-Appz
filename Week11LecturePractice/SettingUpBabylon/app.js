f69450921616c1e162d20874b17cad2720c0b4d0
253olivares <67510851+253olivares@users.noreply.github.com>
2020-11-17 13:52:21 -0500

    note

--- Week11LecturePractice/SettingUpBabylon/app.js
+++ Week11LecturePractice/SettingUpBabylon/app.js
@@ -3,13 +3,13 @@ var canvas = document.getElementById('myCanvas');
 var engine =  new BABYLON.Engine(canvas, true);
 var camera
 
-var createScene = function () {
+function createScene () {
     var scene =  new BABYLON.Scene(engine);
 
     camera = new BABYLON.ArcRotateCamera("Camera". Math.PI/ 2, Math.PI/4,4,BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
 
-    var light = new BABYLON.DirectionalLight()
+    var light = new BABYLON.DirectionalLight('light', new BABYLON.Vector3(0, -0.5,1.0), scene);
 
     var ground = BABYLON.MeshBuilder.CreateGround("ground",{height:4, width: 4, subdivisions: 4}, scene);
     return scene;
