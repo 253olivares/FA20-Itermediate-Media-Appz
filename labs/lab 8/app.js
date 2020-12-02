6cf2137cffb2895ef2cb3340bffdbe16c1971016
Madmanz13579 <67510851+253olivares@users.noreply.github.com>
2020-12-02 13:13:39 -0500

    Updated Pathing for my Io

---
+++ labs/lab 8/app.js
@@ -0,0 +1,29 @@
+function setup(){
+    let cnv = createCanvas(500, 130);
+    cnv.position(0,0);
+    let button = createButton('Go Back');
+    button.position(10, 70);
+    button.style('font-size', '25px');
+    button.style('background', 'none');
+    button.style('border','none');
+    button.style('color','white');
+    button.style('cursor','pointer');
+    button.mouseClicked(goBack);
+    //this is just to link back to main page
+    function goBack(){
+        window.open('../../index.html', '_self');
+    } 
+}   
+
+function draw(){
+    //copy pasted from previous lab to create go back link and title of this lab
+    noStroke();
+    fill(102, 102, 102);
+    rect(0,0,240,60);
+    rect(0,60,125,50);
+    triangle(240,0,270,0,240,60 );
+    triangle(125,60,160,60,125,110 );
+    fill(255,255,255);
+    textSize(32);
+    text("Data Store Lab", 10, 40);
+}
